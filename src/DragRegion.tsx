2ff3205d74da4662cd5394ba44592d54c4262c63
TechPandaPro <javaisntcoffee@gmail.com>
2024-08-15 02:34:58 -0700

    Add window vibrancy, add drag region, add dock menu

---
+++ src/DragRegion.tsx
@@ -0,0 +1,3 @@
+export default function DragRegion() {
+  return <div className="dragRegion"></div>;
+}
--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,15 @@
 // import { createRoot } from "react-dom/client";
 
+import DragRegion from "./DragRegion";
+
 // const root = createRoot(document.body);
 // root.render(<h2>Hello from React!</h2>);
 
 export default function App() {
-  return <h2>Hello, World!</h2>;
+  return (
+    <>
+      <DragRegion />
+      <h2>Hello, World!</h2>
+    </>
+  );
 }
