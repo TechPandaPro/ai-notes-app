051fbb5fb1d29c613413cf5df892d9b89cc574ef
TechPandaPro <javaisntcoffee@gmail.com>
2024-08-15 01:11:28 -0700

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+// import { createRoot } from "react-dom/client";
+
+// const root = createRoot(document.body);
+// root.render(<h2>Hello from React!</h2>);
+
+export default function App() {
+  return <h2>Hello, World!</h2>;
+}
