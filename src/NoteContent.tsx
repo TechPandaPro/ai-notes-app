2c0485d742c12bb5fc58c6ba54d722e214781bf5
TechPandaPro <javaisntcoffee@gmail.com>
2024-08-17 01:18:44 -0700

    Experiment with NoteContent component

--- src/NoteContent.tsx
+++ src/NoteContent.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, isValidElement, useState } from "react";
 
 interface Segment {
   key: string;
@@ -9,19 +9,57 @@ export default function NoteContent() {
   const [segments, setSegments] = useState<Segment[]>([]);
 
   function handleInput(e: FormEvent<HTMLDivElement>) {
-    console.log(e.target);
+    const target = e.target as HTMLElement;
+    console.log(target);
     console.log("Hey!");
+    if (target.innerText === target.innerHTML) {
+      const targetText = target.innerText;
+      target.innerHTML = "";
+      const newParagraph = document.createElement("p");
+      newParagraph.innerText = targetText;
+      target.append(newParagraph);
+      // setSegments([]);
+    }
+    // for (const elem of target.querySelectorAll("br")) elem.remove();
+    // const targets = target.querySelectorAll("p, div, h2, br")
     setSegments(
-      Array.from((e.target as HTMLElement).querySelectorAll("div, h2")).map(
-        (elem) => ({
-          key: (elem as HTMLElement).dataset.key ?? Date.now().toString(),
-          text: (elem as HTMLElement).innerText,
-        })
-      )
+      Array.from(target.querySelectorAll("p, div, h2, br")).map((_elem) => {
+        const elem = _elem as HTMLElement;
+        if (!elem.dataset.key) elem.dataset.key = Date.now().toString();
+        const newInfo = {
+          // key: elem.dataset.key ?? Date.now().toString(),
+          key: elem.dataset.key,
+          text: elem.innerText,
+        };
+        // elem.dataset.key = newInfo.key;
+        console.log(isValidElement(elem));
+        return newInfo;
+      })
     );
+    for (const elem of target.querySelectorAll(":not([data-registered])")) {
+      elem.remove();
+      console.log(elem);
+    }
+    console.log("segments:");
     console.log(segments);
     // (e.target as HTMLElement).innerHTML = "";
+    // for (const elem of target.querySelectorAll("p, div, h2")) {
+    //   console.log(elem);
+    //   elem.remove();
+    // }
   }
+
+  console.log("after render:");
+  console.log(segments);
+  console.log(segments.map((s) => s.key));
+
+  console.log(
+    segments.map((segment) => (
+      <p key={segment.key} data-key={segment.key} data-registered={true}>
+        {segment.text}
+      </p>
+    ))
+  );
 
   return (
     // <div className="noteContent">
@@ -35,7 +73,12 @@ export default function NoteContent() {
     >
       {/* <h2>Hello, World!</h2> */}
       {segments.map((segment) => (
-        <p key={segment.key} data-key={segment.key}>
+        <p
+          key={segment.key}
+          data-key={segment.key}
+          data-registered={true}
+          // className="section"
+        >
           {segment.text}
         </p>
       ))}
